443bcb71e6a4fcd410636a4248e889dacef98be1
anshikasen <73756447+anshikasen@users.noreply.github.com>
2021-05-18 19:49:50 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,4 @@
+$(window).on('load', function() { // makes sure the whole site is loaded
+$('.preloader').fadeOut(1000); // will first fade out the loading animation
+$('.content').fadeIn(1000); // will fade out the white DIV that covers the website.
+})
